ea8d07cafa9bbfbb25633db30a38b47928e0e8aa
Jailson Lima <jailsoncamargo@gmail.com>
2018-05-22 23:24:59 -0300

    Inserindo uma nova Categoria

--- src/main/java/com/jailsonlima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jailsonlima/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.jailsonlima.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jailsonlima.cursomc.domain.Categoria;
 import com.jailsonlima.cursomc.services.CategoriaService;
@@ -23,6 +27,20 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	/* @RequestBody faz o Json ser convertido para o
+	 * objeto java automaticamente
+	 *  
+	 *  */
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		/* pega a uri do novo recurso que foi inserido */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
