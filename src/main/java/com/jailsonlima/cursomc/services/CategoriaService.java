1a134c936929df0704a714dfe089e2d37511e035
Jailson Lima <jailsoncamargo@gmail.com>
2018-05-25 23:04:54 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/jailsonlima/cursomc/services/CategoriaService.java
+++ src/main/java/com/jailsonlima/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jailsonlima.cursomc.domain.Categoria;
+import com.jailsonlima.cursomc.dto.CategoriaDTO;
 import com.jailsonlima.cursomc.repositories.CategoriaRepository;
 import com.jailsonlima.cursomc.services.exceptions.DateIntegrityException;
 import com.jailsonlima.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
