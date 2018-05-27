0ee536cc0a3a754f47950be092806cbeafa3a6ed
Jailson Lima <jailsoncamargo@gmail.com>
2018-05-27 00:26:14 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/jailsonlima/cursomc/services/ClienteService.java
+++ src/main/java/com/jailsonlima/cursomc/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DateIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DateIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
