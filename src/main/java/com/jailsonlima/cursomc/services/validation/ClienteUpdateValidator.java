4db0a6c7ad7165cd9520fa77bd86177a62d01352
Jailson Lima <jailsoncamargo@gmail.com>
2018-05-27 22:15:15 -0300

    Validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/com/jailsonlima/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,59 @@
+package com.jailsonlima.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.jailsonlima.cursomc.domain.Cliente;
+import com.jailsonlima.cursomc.dto.ClienteDTO;
+import com.jailsonlima.cursomc.repositories.ClienteRepository;
+import com.jailsonlima.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		/* realiza a validação para ver se encontrou algum cliente
+		 * ou seja, está sendo atualizado o cliente, verifica-se contém um e-mail
+		 * que já tinha em um outro cliente do banco de dados, não
+		 * permitindo deixar o e-mail se repetir  */
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+
+	}
+
+}
