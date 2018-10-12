076bae9b757fd8f3ce3a8c4771208c0940ecf357
Jailson Lima <jailsoncamargo@gmail.com>
2018-10-12 17:57:43 -0300

    Inserindo Pedido

--- src/main/java/com/jailsonlima/cursomc/resources/PedidoResource.java
+++ src/main/java/com/jailsonlima/cursomc/resources/PedidoResource.java
@@ -1,11 +1,16 @@
 package com.jailsonlima.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jailsonlima.cursomc.domain.Pedido;
 import com.jailsonlima.cursomc.services.PedidoService;
@@ -23,6 +28,15 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Validated @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		/* pega a uri do novo recurso que foi inserido */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/jailsonlima/cursomc/services/PedidoService.java
+++ src/main/java/com/jailsonlima/cursomc/services/PedidoService.java
@@ -1,24 +1,63 @@
 package com.jailsonlima.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jailsonlima.cursomc.domain.ItemPedido;
+import com.jailsonlima.cursomc.domain.PagamentoComBoleto;
 import com.jailsonlima.cursomc.domain.Pedido;
+import com.jailsonlima.cursomc.domain.enums.EstadoPagamento;
+import com.jailsonlima.cursomc.repositories.ItemPedidoRepository;
+import com.jailsonlima.cursomc.repositories.PagamentoRepository;
 import com.jailsonlima.cursomc.repositories.PedidoRepository;
 import com.jailsonlima.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
